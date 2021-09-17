b8de987f0b8901309012075c2cd2b9f5546436c4
samuel_parkin <samuelparkin1@gmail.com>
2021-09-17 10:41:41 +1000

    got repr and action

--- classes.py
+++ classes.py
@@ -2,21 +2,35 @@ import random
 
 ######## fruit Classes #######
 class Fruit():
-     def __init__(self):
+    def __init__(self):
          self.flavour, self.colour = random.choice(self.varieties)
+    def __repr__(self):
+        return f"<{self.flavour}, {self.colour} , {self.__class__.__name__}>"
 
 class Apple(Fruit):
-    varieties = [("sour", "green"),("sweet", "Red")]
+    varieties = [("sour", "green"),("sweet", "red")]
 
 
 class Pear(Fruit):
-    varieties = [("mellow", "yellow"),("sharp", "Green")]
+    varieties = [("mellow", "yellow"),("sharp", "green")]
     
 ######### tree Classes #####
 
 class Tree():
     def __init__(self):
         self.fruits = []
+
+    def __repr__(self):
+        return f"{self.fruit_type.__name__} tree"
+
+    def blossom(self):
+        for i in range(self.fecundity):
+            self.fruits.append(self.fruit_type())
+            
+    def harvest(self):
+        crop = self.fruits
+        self.fruits = []
+        return crop                          
 
 class Appletree(Tree):
     fecundity = 8
@@ -34,6 +48,10 @@ class Cider():
             "sweet": 0,
             "sour":0, 
             "mellow":0,
-            
+            "sharp": 0,}
 
-        }
+        for fruit in fruitlist:
+            self.flavour[fruit.flavour]+=1
+
+    def __repr__(self):
+        return f"a barrel of {max(self.flavour, key=lambda key: self.flavour[key])} cider"       
